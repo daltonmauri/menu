25748edf1860880ecd96799744704d2183de4ce7
Dalton Mauri <93494887+daltonmauri@users.noreply.github.com>
2022-04-06 16:56:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function openNav() {
+    let opennavbar = document.getElementById("myNav").style.width = '15%';
+   
+}
+
+
+function closeNav() {
+    let closenavbar = document.getElementById("myNav").style.width = '0%';
+}
